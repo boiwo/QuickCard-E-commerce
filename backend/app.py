d23f662dff05a9d6d41218317168cd37888c42ea
benard <benardboiwo703@gmail.com>
2025-10-03 20:51:31 +0300

    update backend

--- backend/app.py
+++ backend/app.py
@@ -191,5 +191,5 @@ def contact_detail(id):
 # ----------------- RUN -----------------
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+        db.create_all()  # create tables if they don’t exist
+    app.run(debug=True, host="0.0.0.0", port=5000)
